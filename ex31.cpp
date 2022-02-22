70eff1129788f8eb5680e79366c26011559268f4
111J531 <100131324+111J531@users.noreply.github.com>
2022-02-22 19:08:18 +0800

    Create ex31.cpp

---
+++ ex31.cpp
@@ -0,0 +1,18 @@
+/*380059 099829*/
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+    printf("380059 099829\n");
+    printf("Hello World\n");
+    printf("hello William\n");
+  int myAge = 15;
+  int myheightm = 1;
+  int myheightcm = 67;
+  int myweight = 73;
+  cout << "I am " << myAge << " years old\n";
+  cout << "My height is " << myheightm << " m " << myheightcm << "cm\n";
+  cout << "My weight is " << myweight << " kg\n";
+  return 0;
+}
