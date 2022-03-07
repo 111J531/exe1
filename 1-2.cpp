304450c8ad6c32bcebf5616424fcb65d158db68e
111J531 <100131324+111J531@users.noreply.github.com>
2022-03-07 19:27:24 +0800

    Create 1-2.cpp

---
+++ 1-2.cpp
@@ -0,0 +1,17 @@
+/******************************************************************************
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+     printf(" hello Selena "); 
+     printf(" my height is %d m %d cm \n",1,80);
+     printf(" this building is %60d cm \n", 890);
+     printf(" this building is %60d cm \n", 7890);
+     printf(" this building is %60d cm \n", 67890);
+     printf(" this building is %60d cm \n", 567890);
+     printf(" this building is %60d cm \n", 1234567890);
+     printf(" pi = %1.14f \n ", 3.141592653589793238462643383279502884197169);
+}
